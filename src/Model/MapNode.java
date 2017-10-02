f307da0c6ca0b317f9144e748c0e6f25b372b2c5
Gurpreet Raju <gurpreetraj92@gmail.com>
2017-10-02 19:57:37 -0400

    Map Reader Classes

---
+++ src/Model/MapNode.java
@@ -0,0 +1,43 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class MapNode {
+	
+	private String continentName;
+	private ArrayList<CountryNode> countries;
+	private int controlValue;
+	
+	public MapNode(String newName,ArrayList<CountryNode> newCountries,int newValue)
+	{
+		this.continentName = newName;
+		this.countries = newCountries;
+		this.controlValue = newValue;
+	}
+	
+	public String getContinentName()
+	{
+		return this.continentName;
+	}
+	
+	public CountryNode[] getCountries()
+	{
+		return this.countries.toArray(new CountryNode[this.countries.size()]);
+	}
+	
+	public int getControlValue(){
+		return this.controlValue;
+	}
+	
+	public void setCountries(ArrayList<CountryNode> newCountries){
+		this.countries = newCountries;
+	}
+	
+	public void addCountry(CountryNode newCountry){
+		if(this.countries==null){
+			this.countries = new ArrayList<CountryNode>();
+		}
+		this.countries.add(newCountry);
+	}
+	
+}
