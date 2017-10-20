af94cb871e863d5a841be14789b7eb46ff0ea37d
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2017-10-20 03:36:36 -0400

    Reinforcement test

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -6,10 +6,12 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-			TestFortification.class, 
-			TestMapFile.class, 
-			TestMapValidation.class,
-			TestPlayer.class })
+				TestFortification.class, 
+				TestMapFile.class, 
+				TestMapValidation.class,
+				TestPlayer.class,
+				TestReinforcement.class
+			})
 
 public class AllTests {
 }
