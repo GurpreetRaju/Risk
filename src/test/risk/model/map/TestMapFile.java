60817717fef5b90e8c95cad4b79f21d8bcc7ef60
Harinder Kaur <snazyahuja11@gmail.com>
2017-11-06 13:23:17 -0500

    Javadoc for all Test classes

--- src/test/risk/model/map/TestMapFile.java
+++ src/test/risk/model/map/TestMapFile.java
@@ -9,15 +9,29 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import org.junit.Before;
 
+/**
+ * This test class tests the MapReader class functions.
+ * @author Gurpreet
+ */
 public class TestMapFile {
 	
+	/**
+	 * MapReader type reference variable to store its object.
+	 */
 	private MapReader mapread;
 	
+	/**
+	 * This function is called before each test case is run.
+	 * @throws Exception any throwable exception.
+	 */
 	@Before
 	public void setUp() throws Exception {
 		mapread = new MapReader();
 	}
 	
+	/**
+	 * Tests the read function that reads the map file loaded from conquest game site.
+	 */
 	@Test
 	public void testRead() {
 		ArrayList<MapNode> actual = mapread.readMap(".//data//test//testMap3.map");
