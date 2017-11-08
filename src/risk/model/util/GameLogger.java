3b72416f135a81a4be7b24050ece86899588bbf6
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2017-11-08 10:56:04 -0500

    Logging window fix

--- src/risk/model/util/GameLogger.java
+++ src/risk/model/util/GameLogger.java
@@ -1,13 +1,10 @@
 package risk.model.util;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,7 +21,7 @@ public class GameLogger extends JPanel implements Observer {
 	private static final long serialVersionUID = -8766321280014020596L;
 	
 	/**
-	 * 
+	 * Frame for the logging window.
 	 */
 	private JFrame frame;
 	/**
@@ -44,38 +41,42 @@ public class GameLogger extends JPanel implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		Player current = GameDriver.getInstance().getCurrentPlayer();
+		frame.setLayout(new FlowLayout());
 		/*Startup GameLogger Display.*/
 		if(arg.equals("Startup")){
-			
 			frame.add(new JLabel("<html><div><b>Startup Phase</b></div><br/><br/></html>"));
 		}
 		/*Cards Exchange View Display.*/
 		else if(arg.equals("Cards")){
-			
+			frame.add(new JLabel("<html><p></p><br/><br/></html>"));
 			frame.add(new JLabel("<html><div><b>Cards Exchange</b></div><br/><br/></html>"));
 			frame.add(new JLabel("Player: "));
 			frame.add(new JLabel(current.getName()));
-			frame.add(new JLabel(" can exchange cards to get more armies  "));
+			frame.add(new JLabel(" can exchange cards to get more armies.  "));
 		}
 		/*Reinforcement Game Logger Display.*/
 		else if(arg.equals("Reinforcement")){
-			
-			frame.add(new JLabel("<html><div><b>Reinforcement Phase</b></div><br/><br/></html>"));
-			frame.add(new JLabel("Player:"));
-			frame.add(new JLabel(current.getName()));
-			frame.add(new JLabel("Armies:"));
-			frame.add(new JLabel(String.valueOf(current.getArmiesCount())));
-			frame.add(new JLabel("Countries:"));
-			String[] countries = current.getCountriesNames();
-			for(String s: countries){
-				frame.add(new JLabel(s));
+			frame.add(new JLabel("<html><p></p><br/><br/></html>"));
+			if(!(current.armiesMoved > 0)){
+				frame.add(new JLabel("<html><p><b>Reinforcement Phase</b></p><br/><br/></html>"));
+				frame.add(new JLabel("Player:"));
+				frame.add(new JLabel(current.getName()));
+				frame.add(new JLabel("Armies:"));
+				frame.add(new JLabel(String.valueOf(current.getArmiesCount())));
+				frame.add(new JLabel("Countries:"));
+				String[] countries = current.getCountriesNames();
+				for(String s: countries){
+					frame.add(new JLabel(s));
+				}
+			}
+			if(current.armiesMoved>0){
+				frame.add(new JLabel(String.valueOf(current.armiesMoved)+" armies Reinforced to: "+current.countrySelected));
 			}
 		}
 		/*Attack Game Logger Display.*/
 		else if(arg.toString().contains("Attack")){
-			
-			frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
-			frame.add(new JLabel("<html><div><b>Attack Phase</b></div><br/><br/></html>"));
+			frame.add(new JLabel("<html><p></p><br/><br/></html>"));
+			frame.add(new JLabel("<html><p><b>Attack Phase</b></p><br/><br/></html>"));
 			frame.add(new JLabel("Player: "+ current.getName()));
 			if(arg.toString().length() > 7){
 				String words[] = arg.toString().substring(6).split("<br>");
@@ -86,17 +87,17 @@ public class GameLogger extends JPanel implements Observer {
 		}
 		/*Fortification Game Logger Display.*/
 		else if(arg.equals("Fortification")){
-			
-			frame.setLayout(new FlowLayout());
-			frame.add(new JLabel("<html><div><b>Fortification Phase</b></div><br/><br/></html>"));
+			frame.add(new JLabel("<html><p></p><br/><br/></html>"));
+			frame.add(new JLabel("<html><p><b>Fortification Phase</b></p><br/><br/></html>"));
 			frame.add(new JLabel("Player: "));
 			frame.add(new JLabel(current.getName()));
 		}
 		/*Players Display for Startup Phase.*/
 		else{
-			frame.add(new JLabel("<html><div>Player:</div></html>"));
+			frame.add(new JLabel("<html><p>Player:</p></html>"));
 			frame.add(new JLabel((String) arg));
 		}
+		frame.add(new JLabel("<html><p></p><br/><br/></html>"));
 		System.out.println("Observer");
 		frame.validate();
 	}
