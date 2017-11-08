df16ac16c4b436e0543c280e634436608f1e9c5e
Outreachy <amitt.bhardwj@gmail.com>
2017-11-07 21:43:10 -0500

    World Domiantion View Javadoc

--- src/risk/view/WorldDominationView.java
+++ src/risk/view/WorldDominationView.java
@@ -14,15 +14,24 @@ import javax.swing.JPanel;
 import risk.model.GameDriver;
 import risk.model.Player;
 
+/**
+ * This class implements the World Domination View which shows 
+ * the portion of countries each player occupy.
+ * @author amitt
+ *@version 1.0
+ */
 public class WorldDominationView extends JPanel implements Observer {
 	
 	
 	/**
-	 * 
+	 * Serial Version id for JFrame.
+	 * {@inheritDoc}
 	 */
 	private static final long serialVersionUID = -1084389704252002294L;
 
-
+	/**
+	 * Creates World Domination View on the Main frame
+	 */
 	public WorldDominationView() {
 		JLabel label = new JLabel("<html><p><b>World Domination View</b></p><br/><br/></html>");
 		this.setLayout(new FlowLayout());
@@ -30,7 +39,9 @@ public class WorldDominationView extends JPanel implements Observer {
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 	}
 	
-
+	/**
+	 * Observer pattern function for Observers to update when there is a notification from the observable.
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		if(arg.equals("Reinforcement") || arg.equals("Fortification")){
